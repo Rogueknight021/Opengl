9799b12a634a49b3a0a44ead158e9c3c2968320d
Rougeknight <78051661+Rougeknight@users.noreply.github.com>
2021-10-20 00:10:53 +0800

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -3,13 +3,11 @@ using namespace std;
 int main()
 {
 	char src[50], dest[50];
-
+	int i;
 	strcpy(src, "This is source");
 	strcpy(dest, "This is destination");
 
 	strncat(dest, src, 5);
-
-	printf("最终的目标字符串： |%s|", dest);
 
 	return 0;
 }
